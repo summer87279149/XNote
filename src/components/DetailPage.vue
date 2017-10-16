bd309271cb3a6df0644f91d83635ac16b90ae3d5
Admin <1033948449@qq.com>
2017-10-16 17:26:00 +0800

    auto commit

--- src/components/DetailPage.vue
+++ src/components/DetailPage.vue
@@ -1,9 +1,17 @@
 <template>
-  <div class="container">
+  <div class="pagecontainer">
     <navigation :title="title"></navigation>
-
-    详细记录页面
-    训练部位train_kind是：{{$route.params.partid}}
+    <scroll :data="train_kinds_arr"
+            class="listview"
+            ref="listview">
+      <Cascader :data="data" v-model="value1" style="z-index: 101;position: absolute;"></Cascader>
+      <div class="mycharts">
+        <IEcharts v-show="canshow" :option="getOptions()" :loading="chartsLoading" @ready="onReady"
+                  @click="onClick"></IEcharts>
+      </div>
+    </scroll>
+    <p>详细记录页面
+      训练部位train_kind是：{{$route.params.partid}}</p>
   </div>
 </template>
 
@@ -20,17 +28,126 @@
     },
     data() {
       return {
+        color: ['#E0022B', '#E09107', '#A3E00B', '#c23531', '#61a0a8', '#d48265', '#91c7ae', '#ca8622', '#546570', '#c4ccd3'],
+        canshow: true,
+        data: [{
+          value: '0',
+          label: '训练记录',
+        }, {
+          value: '1',
+          label: '动作统计'
+        }, {
+          value: '2',
+          label: '往期记录对比'
+        }
+        ],
+        value1: [],
         title: '详情',
         probeType: 3,
-        listenScroll: true,
         loading: false,
-        train_kinds_arr:[]
+        train_kinds_arr: [],
+        chartsLoading: false
       }
     },
     methods: {
-      scroll(e){
+      onClick() {
 
-      }
+      },
+      onReady() {
+
+      },
+      getOptions() {
+        let dateList = this.getAllDataThisYear()
+        let lunarData = [];
+        for (let i = 0; i < dateList.length; i++) {
+          lunarData.push([
+            dateList[i],
+            1
+          ]);
+        }
+        let option = {
+          title: {
+            top: 10,
+            left: 'center',
+            text: '训练记录'
+          },
+          tooltip: {},
+          visualMap: {
+            type: "piecewise",
+            show: true,
+            top: "10",
+            right: '10',
+            pieces: [
+              {gt: 2, label: '休息日', color: "white"},
+              {value: 1, label: '训练日', color: this.color[this.random()]},
+            ],
+          },
+          calendar: {
+            dayLabel: {
+              nameMap: 'cn'
+            },
+            monthLabel: {
+              nameMap: 'cn'
+            },
+            orient: 'vertical',
+            top: 70,
+            left: "center",
+            width: "60%",
+            range: this.getThisYear(),
+            itemStyle: {
+              normal: {borderWidth: 0.5}
+            },
+            yearLabel: {show: false}
+          },
+          series: {
+            type: 'heatmap',
+            coordinateSystem: 'calendar',
+            data: this.getAllDataThisYear()
+          }
+        };
+        return option
+      },
+      random() {//随机取整数
+        return Math.floor(Math.random() * 10)
+      },
+      getThisYear() {
+        let dateinstance = new Date()
+        return dateinstance.getFullYear()
+      },
+      getAllDataThisYear() {
+        function getDate(datestr) {
+          let temp = datestr.split("-");
+          let mydate = new Date(temp[0], temp[1], temp[2]);
+          return mydate;
+        }
+
+        let dateinstance = new Date()
+        let start = dateinstance.getFullYear() + "-" + "00-01"
+        let end = (dateinstance.getFullYear() + 1) + "-" + "00-01"
+        let startTime = getDate(start);
+        let endTime = getDate(end);
+        let results = []
+        while ((endTime.getTime() - startTime.getTime()) >= 0) {
+          let year = startTime.getFullYear();
+          let month = startTime.getMonth().toString().length == 1 ? "0" + startTime.getMonth().toString() : startTime.getMonth();
+          let day = startTime.getDate().toString().length == 1 ? "0" + startTime.getDate() : startTime.getDate();
+//          console.log("循环天数:",year+"-"+(parseInt(month)+1)+"-"+day)
+          let dataStr = year + "-" + (parseInt(month) + 1) + "-" + day
+          results.push(dataStr)
+          startTime.setDate(startTime.getDate() + 1);
+        }
+        return results;
+      },
+      FormatDate(strTime) {
+        let date = new Date(strTime);
+        return date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+      },
+      getCurrentYearMonth() {
+        let myDate = new Date();
+        myDate.getFullYear(); //获取完整的年份(4位,1970-????)
+        myDate.getMonth(); //获取当前月份(0-11,0代表1月)
+        return myDate.getFullYear() + "-" + (myDate.getMonth() + 1)
+      },
     },
     props: {
       param: {}
@@ -43,7 +160,7 @@
 </script>
 
 <style lang="less" scoped>
-  .container {
+  .pagecontainer {
     position: fixed;
     z-index: 100;
     top: 20px;
@@ -52,12 +169,16 @@
     right: 0;
     background: deepskyblue;
     .listview {
+      /*height: 100%;*/
       position: absolute;
       top: 64px;
       bottom: 0;
       left: 0;
       right: 0;
       overflow: hidden;
+    }
+    .mycharts {
+      height: 2000px;
     }
   }
 </style>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="container">
     <!--<keep-alive>-->
-      <router-view></router-view>
+    <router-view></router-view>
     <!--</keep-alive>-->
-    <Navigation :title="title" :showbackbtn="false" :showaddbtn="true" :showSetting="true" @showSetting="pushSetting" @adda="addAction"></Navigation>
+    <Navigation :title="title" :showbackbtn="false" :showaddbtn="true" :showSetting="true" @showSetting="pushSetting"
+                @adda="addAction">
+    </Navigation>
     <scroll @scroll="scroll"
             :listen-scroll="listenScroll"
             :probe-type="probeType"
@@ -11,25 +13,23 @@
             class="listview"
             ref="listview">
       <ul class="tableview">
-
         <li class="cell" v-for="(item,index) in train_kinds_arr">
           <div class="header">
             <p class="title">{{item.name}}</p>
-            <div class="detailbtn" @click="pushToDetail(item)">查看详情 ></div>
+            <div class="detailbtn" @click="pushToDetail(item)">查看详情 >
+            </div>
           </div>
           <div class="contentview">
-            <IEcharts v-if="canshow" :option="getOptions(index)" :loading="chartsLoading" @ready="onReady" @click="onClick"></IEcharts>
+            <IEcharts v-if="canshow" :option="getOptions(index)" :loading="chartsLoading" @ready="onReady"
+                      @click="onClick"></IEcharts>
           </div>
         </li>
       </ul>
-
     </scroll>
-
     <Spin v-show="loading" fix>
-    <Icon type="load-c" size=18 class="demo-spin-icon-load"></Icon>
-    <div>Loading</div>
-  </Spin>
-
+      <Icon type="load-c" size=18  class="demo-spin-icon-load"></Icon>
+      <div>Loading</div>
+    </Spin>
   </div>
 </template>
 
@@ -38,7 +38,12 @@
   import Scroll from '../base/scroll.vue'
   import Navigation from '../base/Navigation.vue'
   import {getTrainKinds, getTrainDateAndID} from '../httprequest/api'
-  import {setUserId, getUserId,storageLocalAllDataThisYear,getStorageLocalAllDataThisYear} from '../httprequest/userdefault'
+  import {
+    setUserId,
+    getUserId,
+    storageLocalAllDataThisYear,
+    getStorageLocalAllDataThisYear
+  } from '../httprequest/userdefault'
 
   export default {
     components: {
@@ -48,9 +53,9 @@
     },
     data() {
       return {
-        counts:0,//记录网络请求的成功数量,因为这里</IEcharts>组件的option是异步获取的，而且</IEcharts>在v-for循环内部，所以要用一个标志位，等待数据全部加载完毕，再加载</IEcharts>组件
-        canshow:false,
-        color: ['#E0022B', '#E09107', '#A3E00B','#c23531', '#61a0a8', '#d48265', '#91c7ae',  '#ca8622', '#546570', '#c4ccd3'],
+        counts: 0,//记录网络请求的成功数量,因为这里</IEcharts>组件的option是异步获取的，而且</IEcharts>在v-for循环内部，所以要用一个标志位，等待数据全部加载完毕，再加载</IEcharts>组件
+        canshow: false,
+        color: ['#E0022B', '#E09107', '#A3E00B', '#c23531', '#61a0a8', '#d48265', '#91c7ae', '#ca8622', '#546570', '#c4ccd3'],
         title: "主页",
         train_kinds_arr: [],//
         /*
@@ -67,19 +72,19 @@
         probeType: 3,
         listenScroll: true,
         loading: true,
-        chartsLoading:true,
+        chartsLoading: true,
       }
     },
     methods: {
-      pushToDetail(item){
-        if(item){
-          this.$router.push({ name: 'detail', params: { partid:item.train_kind }})
+      pushToDetail(item) {
+        if (item) {
+          this.$router.push({name: 'detail', params: {partid: item.train_kind}})
         }
       },
-      addAction(){
-        this.$router.push({ path: '/actionmanage' })
+      addAction() {
+        this.$router.push({path: '/actionmanage'})
       },
-      getOptions(index){
+      getOptions(index) {
         let dateList = this.getAllDataThisYear()
         let lunarData = [];
         for (let i = 0; i < dateList.length; i++) {
@@ -94,26 +99,26 @@
 //        console.log("this.getCurrentYearMonth()",this.getCurrentYearMonth())
 //        console.log("this.train_kinds_arr[index].train_days",this.train_kinds_arr[index].train_days)
 //        console.log("\n,\n")
-       let bar = {
+        let bar = {
           title: {
-            text: "本月"+this.train_kinds_arr[index].name+'记录图',
-            textStyle:{
+            text: "本月" + this.train_kinds_arr[index].name + '记录图',
+            textStyle: {
               fontSize: 15,
             }
           },
           tooltip: {},
           visualMap: {
             type: "piecewise",
-              show: true,
-              pieces: [
-                {gt: 2,label: '休息日',color: "white"},
-                {value: 1, label: '训练日', color: this.color[this.random()]},
+            show: true,
+            pieces: [
+              {gt: 2, label: '休息日', color: "white"},
+              {value: 1, label: '训练日', color: this.color[this.random()]},
             ],
           },
           calendar: {
             range: this.getCurrentYearMonth(),
-              orient: 'vertical',
-              dayLabel: {
+            orient: 'vertical',
+            dayLabel: {
               nameMap: 'cn'
             },
             monthLabel: {
@@ -123,7 +128,7 @@
               show: false
             },
             width: "65%",
-              height: "120px"
+            height: "120px"
           },
           series: [{
             type: 'scatter',//
@@ -135,7 +140,7 @@
                 formatter: function (params) {
 //                  console.log("params是",params)
                   let d = new Date(params.value[0]);
-                  return d.getDate() ;
+                  return d.getDate();
                 },
                 textStyle: {
                   color: '#000'
@@ -143,42 +148,43 @@
               }
             },
             data: lunarData
-          },{
+          }, {
             type: 'heatmap',
-              coordinateSystem: 'calendar',
-              data: this.train_kinds_arr[index].train_days,
-              tooltip:{
-                formatter: function (params) {
+            coordinateSystem: 'calendar',
+            data: this.train_kinds_arr[index].train_days,
+            tooltip: {
+              formatter: function (params) {
 //                  console.log("tooltip字符=",params)
 //                  let d = new Date(params.value[0]);
-                  return params.value[0] ;
-                },
-              }
+                return params.value[0];
+              },
+            }
           }]
         }
         return bar
       },
       //获取本年度所有日期:["2017-01-01","2017-01-02","2017-01-03"....]
-      getAllDataThisYear(){
-        function getDate(datestr){
+      getAllDataThisYear() {
+        function getDate(datestr) {
           let temp = datestr.split("-");
-          let mydate = new Date(temp[0],temp[1],temp[2]);
+          let mydate = new Date(temp[0], temp[1], temp[2]);
           return mydate;
         }
+
         let dateinstance = new Date()
         let start = dateinstance.getFullYear() + "-" + "00-01"
-        let end = (dateinstance.getFullYear()+1) + "-" + "00-01"
+        let end = (dateinstance.getFullYear() + 1) + "-" + "00-01"
         let startTime = getDate(start);
         let endTime = getDate(end);
         let results = []
-        while((endTime.getTime()-startTime.getTime())>=0){
+        while ((endTime.getTime() - startTime.getTime()) >= 0) {
           let year = startTime.getFullYear();
-          let month = startTime.getMonth().toString().length==1?"0"+startTime.getMonth().toString():startTime.getMonth();
-          let day = startTime.getDate().toString().length==1?"0"+startTime.getDate():startTime.getDate();
+          let month = startTime.getMonth().toString().length == 1 ? "0" + startTime.getMonth().toString() : startTime.getMonth();
+          let day = startTime.getDate().toString().length == 1 ? "0" + startTime.getDate() : startTime.getDate();
 //          console.log("循环天数:",year+"-"+(parseInt(month)+1)+"-"+day)
-          let dataStr = year+"-"+(parseInt(month)+1)+"-"+day
+          let dataStr = year + "-" + (parseInt(month) + 1) + "-" + day
           results.push(dataStr)
-          startTime.setDate(startTime.getDate()+1);
+          startTime.setDate(startTime.getDate() + 1);
         }
 
         return results;
@@ -187,7 +193,7 @@
         let date = new Date(strTime);
         return date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
       },
-      getCurrentYearMonth(){
+      getCurrentYearMonth() {
         let myDate = new Date();
         myDate.getFullYear(); //获取完整的年份(4位,1970-????)
         myDate.getMonth(); //获取当前月份(0-11,0代表1月)
@@ -212,36 +218,32 @@
       param: {}
     },
     computed: {},
-    watch: {
-
-
-    },
+    watch: {},
     created() {
       this.probeType = 3
       this.listenScroll = true
     },
 
     mounted() {
-
       this.getAllDataThisYear()
       getTrainKinds(getUserId()).then(res => {
         if (res.code == 200) {
           let arr = []
-          let dataCounts  =  res.data.length
+          let dataCounts = res.data.length
 //          console.log("dataCounts=",dataCounts)
           for (let obj of res.data) {
             getTrainDateAndID(getUserId(), obj.train_kind).then(res => {
               let timeArr = []
               let a;
-              for (let obj2 of res.data){
-                a = [this.FormatDate(obj2.cteate_time),1]
+              for (let obj2 of res.data) {
+                a = [this.FormatDate(obj2.cteate_time), 1]
                 timeArr.push(a)
               }
 //              console.log("最终包装的日期:",timeArr)
               obj.train_days = timeArr
               this.counts++
 //              console.log("this.counts=",this.counts)
-              if (this.counts == dataCounts){
+              if (this.counts == dataCounts) {
 //                console.log("相等了",dataCounts,this.counts)
                 this.canshow = true
               }
@@ -286,19 +288,28 @@
 </script>
 
 <style lang="less" scoped>
-  .demo-spin-icon-load{
+  .demo-spin-icon-load {
     animation: ani-demo-spin 1s linear infinite;
   }
+
   @keyframes ani-demo-spin {
-    from { transform: rotate(0deg);}
-    50%  { transform: rotate(180deg);}
-    to   { transform: rotate(360deg);}
+    from {
+      transform: rotate(0deg);
+    }
+    50% {
+      transform: rotate(180deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
   }
-  .demo-spin-col{
+
+  .demo-spin-col {
     height: 100px;
     position: relative;
     border: 1px solid #eee;
   }
+
   .container {
     .listview {
       position: absolute;
